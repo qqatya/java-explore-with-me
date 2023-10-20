2d36fdba0e018297136ff1ca4ab23fa82d02b7fb
qqatya <catherineburger@yandex.ru>
2023-10-21 01:32:52 +0300

    fix: добавить аннотации @Repository

--- ewm-stats/stats-server/src/main/java/ru/practicum/repository/HitRepository.java
+++ ewm-stats/stats-server/src/main/java/ru/practicum/repository/HitRepository.java
@@ -2,8 +2,10 @@ package ru.practicum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import ru.practicum.entity.Hit;
 
+@Repository
 public interface HitRepository extends JpaRepository<Hit, Long> {
 
     @Query(value = "SELECT COUNT(DISTINCT ip_address) FROM hits WHERE endpoint_id = ?1", nativeQuery = true)
